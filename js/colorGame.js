c2d54c325f97af61154472042353331ee436417c
Beatrice <beatriceopara94@gmail.com>
2019-09-17 16:26:17 +0100

    Update colorGame.js
    created an variable with array of 6 rgb colors

--- js/colorGame.js
+++ js/colorGame.js
@@ -1,1 +1,28 @@
-alert("CONNECTED");
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll(".square");
+// six squares and our i is going to loop through array 0 to 5.
+// this will loop through each square
+// e.g. 0 is the first div and 1 is the second square div
+
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+  //add inital colors to squares
+  squares[i].style.backgroundColor = colors[i];
+
+  //add click listeners to squares
+  squares[i].addEventListener("click", function(){
+    alert("clicked a square");
+  });
+}

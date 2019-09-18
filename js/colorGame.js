ecb5eda80b52d48744b5ef2a3f660736c6ff0905
Beatrice <beatriceopara94@gmail.com>
2019-09-18 01:10:13 +0100

    styling
    styled elements

--- js/colorGame.js
+++ js/colorGame.js
@@ -56,7 +56,7 @@ colorDisplay.textContent = pickedColor;
 for(var i = 0; i < squares.length; i++){
   squares[i].style.backgroundColor = colors[i];
   };
-h1.style.backgroundColor = "#232323";
+h1.style.backgroundColor = "steelblue";
 })
 
 colorDisplay.textContent = pickedColor;
